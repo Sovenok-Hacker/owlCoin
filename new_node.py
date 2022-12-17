17cd66232bafc46d3803d268027988c413982173
Artem <79601745+Sovenok-Hacker@users.noreply.github.com>
2022-12-17 17:40:28 +0000

    Add files via upload

--- new_node.py
+++ new_node.py
@@ -1,44 +1,39 @@
-import json, time, hashlib, random, requests
+import json, time, hashlib, random, requests, plyvel
+from waitress import serve
 from flask import Flask, request
 from ecdsa import SigningKey, VerifyingKey, NIST384p, BadSignatureError
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 app = Flask('owlCoin')
 limiter = Limiter(app, key_func=get_remote_address)
-blockchain = []
+db = plyvel.DB('db', create_if_missing=True)
+def get_block(index=1):
+    return json.loads(db.get(str(index).encode()))
+def get_length():
+    i = 0
+    for k, v in db:
+        i += 1
+    return i
+if not get_length():
+    db.put(b'1', b'[[], 0, null, null, [null, null, null]]')
 class systemData():
     def __init__(self):
-        self.mspeed = 0
-        self.diff = {}
         self.tasks = {}
         self.pending_transactions = []
         self.friends = []
 sysData = systemData()
-class Block():
-    def __init__(self, data, timestamp, index, phash, pow):
-        self.data = data
-        self.timestamp = timestamp
-        self.index = index
-        self.phash = phash
-        self.pow = pow
-        self.hash = hashlib.sha256(f'{self.data}{self.timestamp}{self.index}{self.phash}{self.pow}'.encode()).hexdigest()
-        def check_pow():
-            if pow: # if block is not genesis
-                return hashlib.sha256(f'{pow[0] * pow[1]}'.encode()).hexdigest().endswith('0' * pow[2])
-        self.check_pow = check_pow
-blockchain.append(Block({'transactions': [], 'user_data': []}, None, len(blockchain) + 1, None, None)) # create genesis block
 def validate(chain):
-    thash = chain[0].hash
-    for block in chain[1:]:
-        for txion in block.data['transactions']:
+    thash = chain[1]
+    for block in chain:
+        for txion in block:
             try:
-                VerifyingKey.from_string(bytes.fromhex(txion[0])).verify(bytes.fromhex(txion[4]), hashlib.sha256(
+                VerifyingKey.from_string(bytes.fromhex(txion[0])).verify(bytes.fromhex(txion[4]), hashlib.blake2s(
                     f'{txion[0]}{txion[1]}{txion[2]}{txion[3]}'.encode()).hexdigest().encode())
             except BadSignatureError:
                 return False
-        if not block.phash == thash or not block.check_pow():
+        if not block[3]:
             return False
-        thash = block.hash
+        thash = block[2]
     return True
 @app.route('/announce_nb', methods=['POST'])
 @limiter.limit("20/minute")
@@ -47,9 +42,9 @@ def consesnus():
         block = request.get_json()['block']
     except KeyError:
         return json.dumps({'ok': False, 'message': 'You must specify the block json.'})
-    if not validate(blockchain + [Block(*block)]): # if our chain with this block is invalid
+    if not validate([get_block(get_length()), block]): # if our chain with this block is invalid
         return json.dumps({'ok': False, 'message': 'Block is invalid.'})
-    blockchain.append(Block(*block))
+    blockchain.append(block)
     return {'ok': True}
 @app.route('/meet',methods=['GET','POST']) # add friend
 @limiter.limit("5/minute")
@@ -71,90 +66,80 @@ def meet():
         return {'ok': 'Error in requesting node.'}
 @app.route('/txs',methods=['GET','POST']) # get balance
 @limiter.limit("20/minute")
-def balance():
+def gtxs():
     try:
         addr = request.args['addr']
     except KeyError:
         return json.dumps({'ok': False, 'message': 'You must specify the address.'})
     result = 0
     transactions = []
-    for block in blockchain:
-        for txion in block.data['transactions']:
-            transactions.append(txion)
+    for i in range(1, get_length()):
+        block = get_block(i)
+        for txion in block[0]:
+            if txion[0] == addr or txion[1] == addr:
+                transactions.append(txion)
     return json.dumps({'ok': True, 'txs': transactions})
-@app.route('/balance',methods=['GET','POST']) # get balance
-@limiter.limit("20/minute")
-def balance():
-    try:
-        addr = request.args['addr']
-    except KeyError:
-        return json.dumps({'ok': False, 'message': 'You must specify the address.'})
-    result = 0
-    transactions = []
-    for block in blockchain:
-        if not block.index == 1:
-            if block.pow[3] == addr: # if block mined by this addr
-                result += 1
-        for txion in block.data['transactions']:
-            transactions.append(txion)
-    for txion in transactions:
-        if txion[0] == addr:
-            result -= txion[2]
-        elif txion[1] == addr:
-            result += txion[2]
-    return json.dumps({'ok': True, 'balance': result})
-@app.route('/blocks',methods=['GET','POST'])
-@limiter.limit("30/minute")
+@app.route('/blocks', methods=['GET','POST'])
+@limiter.limit("5/minute")
 def get_blocks():
     answer = []
-    for block in blockchain:
-        answer.append([block.data, block.timestamp, block.index, block.hash, block.phash, block.pow])
-    return json.dumps(answer)
+    for i in range(1, get_length()):
+        data = get_block(i)
+        answer.append([data[0], data[1], i, data[2]])
+    return json.dumps(answer), 200, {'Content-Type': 'application/json; charset=utf-8'}
+@app.route('/block', methods=['GET', 'POST'])
+@limiter.limit("60/minute")
+def gb():
+    try:
+        index = int(request.args['index'])
+    except (KeyError, ValueError):
+        return json.dumps({'ok': False, 'message': 'Please, specify the index.'})
+    if index > get_length():
+        return json.dumps({'ok': False, 'message': 'Can`t find this block.'})
+    return json.dumps(get_block(index))
 @app.route('/transaction',methods=['POST']) # send coins
-@limiter.limit("30/minute")
+@limiter.limit("10/minute")
 def transfer():
     try:
         sign = request.args['sign']
         sender = request.args['from']
         receiver = request.args['to']
         amount = float(request.args['amount'])
-    except KeyError:
+    except (KeyError, ValueError):
         return json.dumps({'ok': False, 'message': 'You must specify the sign, sender address, receiver address, timestamp and amount.'})
     message = request.args.get('message')
     result = 0
     transactions = []
-    for block in blockchain:
-        for txion in block.data['transactions']:
-            transactions.append(txion)
-    for txion in transactions:
-        if txion[0] == sender:
-            result -= txion[2]
-        elif txion[1] == sender:
-            result += txion[2]
+    for i in range(1, get_length()):
+        for txion in get_block(i)[0]:
+            if txion[0] == sender:
+                result -= txion[2]
+            elif txion[1] == sender:
+                result += txion[2]
     if result < amount:
         return json.dumps({'ok': False, 'message': 'You haven`t enougth coins to send.'})
     try:
-        VerifyingKey.from_string(bytes.fromhex(sender)).verify(bytes.fromhex(sign), hashlib.sha256(f'{sender}{receiver}{amount}{message}'.encode()).hexdigest().encode())
+        VerifyingKey.from_string(bytes.fromhex(sender)).verify(bytes.fromhex(sign), hashlib.blake2s(f'{sender}{receiver}{amount}{message}'.encode()).hexdigest().encode())
     except BadSignatureError:
         return json.dumps({'ok': False, 'message': 'Invalid signature.'})
     sysData.pending_transactions.append([sender, receiver, amount, message, sign])
     return json.dumps({'ok': True})
 @app.route('/task',methods=['POST'])
+# @limiter.limit("20/minute")
 def create_task():
     try:
         addr = request.args['addr']
     except KeyError:
         return json.dumps({'ok': False, 'message': 'You must specify your address (to get a reward).'})
-    if sysData.diff.get(addr) == None:
-        sysData.diff.update({addr: 5})
     y = random.randint(0, 1000000)
-    task_id = int(random.randint(0, y * y))
-    sysData.tasks.update({task_id: [y, sysData.diff[addr], time.time(), addr]})
-    for block in blockchain:
-        index = block.index
+    task_id = int(random.randint(0, y ** 2))
+    diff = get_length() // 1000
+    sysData.tasks.update({task_id: [y, diff, time.time(), addr]})
+    index = get_length() + 1
     print(index)
-    return json.dumps([y, sysData.diff[addr], task_id, index])
+    return json.dumps([y, diff, task_id, index])
 @app.route('/check',methods=['POST'])
+# @limiter.limit("20/minute")
 def check():
     try:
         tid = int(request.args['task_id'])
@@ -163,21 +148,23 @@ def check():
         return json.dumps({'ok': False, 'message': 'You must specify the task ID and solution.'})
     if not sysData.tasks.get(tid):
         return json.dumps({'ok': False, 'message': 'Task ID is invalid or task already solved.'})
-    if hashlib.blake2s(f'{sysData.tasks[tid][0] * solution}'.encode()).hexdigest().endswith('0' * sysData.tasks[tid][1]):
+    if hashlib.blake2s(str(sysData.tasks[tid][0] * solution).encode()).hexdigest().endswith('0' * sysData.tasks[tid][1]):
         solved_in = time.time() - sysData.tasks[tid][2]
+        lb = get_block(get_length())
+        nb = json.dumps([sysData.pending_transactions,
+        round(time.time()),
+        hashlib.blake2s(json.dumps(sysData.pending_transactions + [sysData.tasks[tid][0], solution, sysData.tasks[tid][1], sysData.tasks[tid][3]] + [lb[1], lb[2]], ensure_ascii=False).encode()).hexdigest(),
+        lb[2],
+        [sysData.tasks[tid][0], solution, sysData.tasks[tid][1], sysData.tasks[tid][3]]], ensure_ascii=False)
+        db.put(str(get_length() + 1).encode(), nb.encode())
+        sysData.pending_transactions = []
         sysData.pending_transactions.append(['Blockchain', sysData.tasks[tid][3], 1.0, 'Thanks for mining!', 'Blockchain'])
-        blockchain.append(Block({'transactions': sysData.pending_transactions, 'user_data': []}, time.time(), len(blockchain) + 1, blockchain[-1].hash, [sysData.tasks[tid][0], solution, sysData.tasks[tid][1], sysData.tasks[tid][3]]))
         for node in sysData.friends:
             try:
-                requests.post(f'{node}/announce_nb', json={'block': [blockchain[-1].data, blockchain[-1].timestamp, blockchain[-1].index, blockchain[-1].phash, blockchain[-1].pow]})
+                requests.post(f'{node}/announce_nb', json=lb)
             except:
                 pass
-        sysData.pending_transactions = []
-        if solved_in < 10:
-            sysData.diff[sysData.tasks[tid][3]] += 1
-        elif solved_in > 60:
-            sysData.diff[sysData.tasks[tid][3]] -= 1
         sysData.tasks.pop(tid)
         return json.dumps({'ok': True})
     return json.dumps({'ok': False, 'message': f'Solution is incorrect.'})
-app.run(host='0.0.0.0', port=8000, threaded=True)
+serve(app, host='0.0.0.0', port=8000)
